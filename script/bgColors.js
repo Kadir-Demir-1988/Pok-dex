9ccd5c84ecd414242f27b134fb5b9d552885c11b
Kadir Demir <k.demir1988@gmail.com>
2024-07-16 12:46:28 +0200

    typenfarben

---
+++ script/bgColors.js
@@ -0,0 +1,38 @@
+function TypeColorClass(type) {
+  switch (type) {
+    case "grass":
+      return "bgColorgrass";
+    case "fire":
+      return "bgColorfire";
+    case "water":
+      return "bgColorwater";
+    case "bug":
+      return "bgColorbug";
+    case "normal":
+      return "bgColornormal";
+    case "poison":
+      return "bgColorpoison";
+    case "electric":
+      return "bgColorlectric";
+    case "ground":
+      return "bgColorground";
+    case "fairy":
+      return "bgColorfairy";
+    case "fighting":
+      return "bgColorfighting";
+    case "psychic":
+      return "bgColorpsychic";
+    case "rock":
+      return "bgColorrock";
+    case "ice":
+      return "bgColorice";
+    case "flying":
+      return "bgColorflying";
+    case "steel":
+      return "bgColorsteel";
+    case "ghost":
+      return "bgColorpsychic";
+    case "dragon":
+      return "bgColordragon";
+  }
+}
--- script/script.js
+++ script/script.js
@@ -11,7 +11,7 @@ function init() {
 
 async function loadAPI() {
   try {
-    let url = `https://pokeapi.co/api/v2/pokemon?limit=9&offset=0`;
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`;
     let response = await fetch(url);
     let responseAsJson = await response.json();
     await loadPokemonDetails(responseAsJson.results);
@@ -46,26 +46,25 @@ function upperCase(name) {
 }
 
 function showPokemonCards() {
-    let content = document.getElementById("pokecards");
-    content.innerHTML = "";
-    for (let i = 0; i < pokemonDetails.name.length; i++) {
-      let types = pokemonDetails.type[i].split(', ');
-      for (let j = 0; j < types.length; j++) {
-        types[j] = upperCase(types[j]);
-      }
-      content.innerHTML += `
-          <div id="borderid">
-            <h2>${upperCase(pokemonDetails.name[i])} #${pokemonDetails.id[i]}</h2>
-            <div>
-              <img class="pokeimg" src="${pokemonDetails.img[i]}" alt="${pokemonDetails.name[i]}">
-            </div>
-            <div>
-              <p>Type: ${types.join(', ')}</p>
-            </div>
-          </div>
-        `;
+  let content = document.getElementById("pokecards");
+  content.innerHTML = "";
+  for (let i = 0; i < pokemonDetails.name.length; i++) {
+    let types = pokemonDetails.type[i].split(', ');
+    for (let j = 0; j < types.length; j++) {
+      types[j] = upperCase(types[j]);
     }
+    content.innerHTML += `
+        <div id="borderid">
+          <h2>${upperCase(pokemonDetails.name[i])} #${pokemonDetails.id[i]}</h2>
+          <div>
+            <img class="pokeimg" src="${pokemonDetails.img[i]}" alt="${pokemonDetails.name[i]}">
+          </div>
+          <div class="typeContainer">
+            ${types.map(type => `<div class="${TypeColorClass(type.toLowerCase())}">${type}</div>`).join('')}
+          </div>
+        </div>
+      `;
   }
-  
+}
 
 console.log(pokemonDetails);
